9876fce0b863fe0a9fbdcc24b6f5bae55991cb13
Oyuri-Taiko <45296675+Oyuri-Taiko@users.noreply.github.com>
2019-02-18 19:32:52 +0100

    Update Kalkulator 1.0.cpp

--- Kalkulator 1.0.cpp
+++ Kalkulator 1.0.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-auto dodawanie(auto skladnik_1, auto skladnik_2 ) -> double long;
-auto odejmowanie(auto odejmna, auto odejmnik) -> double long;
-auto mnozenie(auto czynnik_1, auto czynnik_2) -> double long;
-auto dzielenie(auto dzielna, auto dzielnik) -> double long;
-auto potega(auto stopien, auto liczba) -> double long;
+double long dodawanie(auto skladnik_1, auto skladnik_2);
+double long odejmowanie(auto odejmna, auto odejmnik);
+double long mnozenie(auto czynnik_1, auto czynnik_2);
+double long dzielenie(auto dzielna, auto dzielnik);
+double long potega(auto podstawa, int stopien);
 //*******************************************************************
 int main()
 {
@@ -20,82 +20,87 @@ int main()
 		 << "\nWybierz numer dzialania : ";
 	int dzialanie;
 	cin >> dzialanie;	
-	switch(dzialanie)
-	{
-		double long a;
-		double long b;
-		case 1:	
-			cout << "\nWybrano dodawanie, podaj wartosc a : ";
+	if(0 < dzialanie < 6)
+		{
+			double long a;
+			double long b;
+		
+			cout << "\nWybrano ";
+			switch(dzialanie)
+					{
+						case 1:	
+						cout << "dodawanie";
+						break; 		 
+						case 2:
+						cout << "odejmowanie"; 
+						break; 
+						case 3:
+						cout << "mnozenie";
+						break; 
+						case 4:
+						cout << "dzielenie";
+						break; 
+						case 5:
+						cout << "potegowanie";
+						break; 
+					}
+			cout <<", podaj wartosc a : ";
 			cin >> a;
 			cout << "\nPodaj wartosc b : ";
 			cin >> b;
-			cout << "\nWprowadzono wartosci a = "  << a <<" b = " << b << " "
-				 << ", dla podanych wartosci suma wynosi = " << dodawanie(a, b) << "\n\n";
-		goto abc;	 		 
-		case 2:
-			cout << "\nWybrano odejmowanie, podaj wartosc a : ";
-			cin >> a;
-			cout << "\nPodaj wartosc b : ";
-			cin >> b;
-			cout << "\nWprowadzono wartosci a = "  << a <<" b = " << b << " "
-				 << ", dla podanych wartosci roznica wynosi = " << odejmowanie(a, b) << "\n\n";
-		goto abc;	 
-		case 3:
-			cout << "\nWybrano mnozenie, podaj wartosc a : ";
-			cin >> a;
-			cout << "\nPodaj wartosc b : ";
-			cin >> b;
-			cout << "\nWprowadzono wartosci a = "  << a <<" b = " << b << " "
-				 << ", dla podanych wartosci iloraz wynosi = " << mnozenie(a, b) << "\n\n";
-		goto abc;	 
-		case 4:
-			cout << "\nWybrano dzielenie, podaj wartosc a : ";
-			cin >> a;
-			cout << "\nPodaj wartosc b : ";
-			cin >> b;
-			cout << "\nWprowadzono wartosci a = "  << a <<" b = " << b << " "
-				 << ", dla podanych wartosci iloraz wynosi = " << mnozenie(a, b) << "\n\n";
-		goto abc;	 
-		case 5:
-			cout << "\nWybrano potegowanie, podaj wartosc podstawy a : ";
-			cin >> a;
-			cout << "\nPodaj wartosc stopnia: ";
-			cin >> b;
-			cout << "\nWprowadzono wartosci a = "  << a <<" b = " << b << " "
-				 << ", dla podanych wartosci potegowanie wynosi = " << potega(b, a) << "\n\n";
-		goto abc;	 
-		default:
-			cout << "\nWybrano nieprawidlowy numer, powrot do menu glownego. \n\n";
+			cout << "\nWprowadzono wartosci a = "  << a <<" b = " << b << " ";
+				switch(dzialanie)
+					{
+						case 1:	
+						cout << ", dla podanych wartosci suma wynosi = " << dodawanie(a, b) << "\n\n";
+						goto abc;	 		 
+						case 2:
+						cout << ", dla podanych wartosci roznica wynosi = " << odejmowanie(a, b) << "\n\n";
+						goto abc;	 
+						case 3:
+						cout << ", dla podanych wartosci iloraz wynosi = " << mnozenie(a, b) << "\n\n";
+						goto abc;	 
+						case 4:
+						cout << ", dla podanych wartosci iloraz wynosi = " << mnozenie(a, b) << "\n\n";
+						goto abc;	 
+						case 5:
+						cout << ", dla podanych wartosci potegowanie wynosi = " << potega(a, b) << "\n\n";
+						goto abc;	 
+					}
+		}
+	
+	else
+				cout << "\nWybrano nieprawidlowy numer, powrot do menu glownego. \n\n";
 		goto abc;
-	}
+		
 }
 //**************************************************************************************
-auto dodawanie(auto skladnik_1, auto skladnik_2 ) -> double long
+double long dodawanie(auto skladnik_1, auto skladnik_2 )
 {
 	return skladnik_1 + skladnik_2;
 }
 //****************************************************************************************
-auto odejmowanie(auto odjemna, auto odjemnik) -> double long
+double long odejmowanie(auto odjemna, auto odjemnik)
 {
 	return  odjemna - odjemnik;
 }
 //******************************************************************************************
-auto mnozenie(auto czynnik_1, auto czynnik_2) -> double long
+double long mnozenie(auto czynnik_1, auto czynnik_2)
 {
 	return czynnik_1 * czynnik_2;
 }
 //***************************************************************************************
-auto dzielenie(auto dzielna, auto dzielnik) -> double long
+double long dzielenie(auto dzielna, auto dzielnik)
 {
 	return dzielna / dzielnik;
 }
 //***************************************************************************************
-auto potega(auto stopien, auto liczba) -> double long
+double long potega(auto podstawa, int stopien)
 {
-	long wynik = liczba;
+	long wynik = podstawa;
 	for(int s = 1 ; s < stopien ; ++s)
 	{
-		wynik = wynik * liczba;
+		wynik = wynik * podstawa;
 	}
 	return wynik;
 }
